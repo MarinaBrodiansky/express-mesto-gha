bcdb6770dc42c3bb9e92c240859f0bbf27a5ca3e
Marina Brodiasky <marina.brodianskaya@yandex.ru>
2023-06-09 20:27:32 +0300

    ПР13tests

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const rateLimit = require('express-rate-limit'); // limiter
 const helmet = require('helmet'); // https://expressjs.com/ru/advanced/best-practice-security.html
-const { LIMITER, MONGO_DB, PORT } = require('./utils/config');
+
+const { PORT = 3000 } = process.env;
+const { MONGO_DB = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const { MSG_NOT_FOUND } = require('./utils/globalVars');
+
+const LIMITER = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
 const app = express();
 app.use(express.json());
